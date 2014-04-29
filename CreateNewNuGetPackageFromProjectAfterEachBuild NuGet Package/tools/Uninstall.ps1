06109c0b81110a8ae1e08290977672244eea2af0
deadlydog <deadlydog@hotmail.com>
2014-04-29 17:58:55 -0600

    - Fix bug to make sure the Post-Build event matches on the install and uninstall scripts to make sure it gets removed on uninstall.

--- CreateNewNuGetPackageFromProjectAfterEachBuild NuGet Package/tools/Uninstall.ps1
+++ CreateNewNuGetPackageFromProjectAfterEachBuild NuGet Package/tools/Uninstall.ps1
@@ -8,7 +8,7 @@ $postBuildEventCode = @'
 REM Create a NuGet package for this project and place the .nupkg file in the project's output directory.
 REM If you see this in Visual Studio's Error List window, check the Output window's Build tab for the actual error.
 ECHO Building NuGet package in Post-Build event...
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '$(ProjectDir)PostBuildScripts\BuildNewPackage-RanAutomatically.ps1' -ProjectFilePath '$(ProjectPath)' -OutputDirectory '$(TargetDir)' -Configuration='$(ConfigurationName)' -Platform='$(PlatformName)'"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '$(ProjectDir)PostBuildScripts\BuildNewPackage-RanAutomatically.ps1' -ProjectFilePath '$(ProjectPath)' -OutputDirectory '$(TargetDir)' -Configuration '$(ConfigurationName)' -Platform '$(PlatformName)'"
 '@
 
 # Remove the Post-Build Event Code to the project and save it.
