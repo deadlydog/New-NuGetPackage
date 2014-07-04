cdc94fc54c4fdefb9b68625b7d7be32f47613dfd
deadlydog <deadlydog@hotmail.com>
2014-07-04 17:05:13 -0600

    Moved Test Files into their own directory and updated Test.ps1 to use it.

--- Tests/Test.ps1
+++ Tests/Test.ps1
@@ -12,57 +12,57 @@ cd $THIS_SCRIPTS_DIRECTORY
 Clear-Host
 
 # Test packing from a project with a NuSpec file.
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -Verbose
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project\TestProject\TestProject.csproj" -VersionNumber "1.1" -ReleaseNotes "Pack project without building and specify version 1.1"
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -Verbose
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project\TestProject\TestProject.csproj" -VersionNumber "1.1" -ReleaseNotes "Pack project without building and specify version 1.1"
 
 # Test packing from a project without a NuSpec file.
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project Without NuSpec\TestProjectWithoutNuSpec\TestProjectWithoutNuSpec.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY"""
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project Without NuSpec\TestProjectWithoutNuSpec\TestProjectWithoutNuSpec.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY"""
 
 # Test packing from a nuspec file.
-#.\..\New-NuGetPackage.ps1 -NuSpecFilePath "$THIS_SCRIPTS_DIRECTORY\Test NuSpec File\StaticNuSpecFileTest.nuspec"
-#.\..\New-NuGetPackage.ps1 -NuSpecFilePath "$THIS_SCRIPTS_DIRECTORY\Test NuSpec File\StaticNuSpecFileTest.nuspec" -VersionNumber "1.0.0" -ReleaseNotes "Pack nuspec file with version number and release notes specified"
+#.\..\New-NuGetPackage.ps1 -NuSpecFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test NuSpec File\StaticNuSpecFileTest.nuspec"
+#.\..\New-NuGetPackage.ps1 -NuSpecFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test NuSpec File\StaticNuSpecFileTest.nuspec" -VersionNumber "1.0.0" -ReleaseNotes "Pack nuspec file with version number and release notes specified"
 
 # Test errors to see how they look.
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project With Errors So It Will Not Pack\ProjectWithErrorsSoItWillNotPack\ProjectWithErrorsSoItWillNotPack.csproj" -PackOptions "-Build"
-#.\..\New-NuGetPackage.ps1 -NuSpecFilePath "$THIS_SCRIPTS_DIRECTORY\Test NuSpec With Errors So It Will Not Pack\NuSpecFileWithErrorsTest.nuspec" -NoPrompt
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project With Errors So It Will Not Pack\ProjectWithErrorsSoItWillNotPack\ProjectWithErrorsSoItWillNotPack.csproj" -PackOptions "-Build"
+#.\..\New-NuGetPackage.ps1 -NuSpecFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test NuSpec With Errors So It Will Not Pack\NuSpecFileWithErrorsTest.nuspec" -NoPrompt
 
 # Test packing without specifying a file path or any other parameters.
 #.\..\New-NuGetPackage.ps1
 #.\..\New-NuGetPackage.ps1 -UsePowerShellPrompts -NoPromptForInputOnError
 
 # Test packing using the various different prompt modes.
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -NoPrompt
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -NoPromptForVersionNumber
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -NoPromptForReleaseNotes
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -NoPromptForPushPackageToNuGetGallery
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -NoPromptForInputOnError
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -UsePowershellPrompts
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -NoPrompt
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -NoPromptForVersionNumber
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -NoPromptForReleaseNotes
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -NoPromptForPushPackageToNuGetGallery
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -NoPromptForInputOnError
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -UsePowershellPrompts
 
 # Test packing without updating the NuSpec file.
-#.\..\New-NuGetPackage.ps1 -NuSpecFilePath "$THIS_SCRIPTS_DIRECTORY\Test NuSpec File\StaticNuSpecFileTest.nuspec" -DoNotUpdateNuSpecFile
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -DoNotUpdateNuSpecFile
+#.\..\New-NuGetPackage.ps1 -NuSpecFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test NuSpec File\StaticNuSpecFileTest.nuspec" -DoNotUpdateNuSpecFile
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project\TestProject\TestProject.csproj" -PackOptions "-Build -OutputDirectory ""$THIS_SCRIPTS_DIRECTORY""" -DoNotUpdateNuSpecFile
 
 # Test packing by specifying a custom NuGet.exe path with spaces.
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project\TestProject\TestProject.csproj" -VersionNumber "1.2" -ReleaseNotes "Pack project without building and specify version 1.2" -NuGetExecutableFilePath "$THIS_SCRIPTS_DIRECTORY\NuGet Executable\NuGet.exe"
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project\TestProject\TestProject.csproj" -VersionNumber "1.2" -ReleaseNotes "Pack project without building and specify version 1.2" -NuGetExecutableFilePath "$THIS_SCRIPTS_DIRECTORY\NuGet Executable\NuGet.exe"
 
 # Test packing from a relative path.
-#.\..\New-NuGetPackage.ps1 -NuSpecFilePath ".\Test NuSpec File\StaticNuSpecFileTest.nuspec" -DoNotUpdateNuSpecFile
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath ".\Test Project\TestProject\TestProject.csproj" -DoNotUpdateNuSpecFile
+#.\..\New-NuGetPackage.ps1 -NuSpecFilePath ".\Test Files\Test NuSpec File\StaticNuSpecFileTest.nuspec" -DoNotUpdateNuSpecFile
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath ".\Test Files\Test Project\TestProject\TestProject.csproj" -DoNotUpdateNuSpecFile
 
 # Test the NuGet Update switch.
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project\TestProject\TestProject.csproj" -VersionNumber "1.3" -ReleaseNotes "Pack project without building and specify version 1.3 and do NOT update the NuGet.exe" -NoPromptForPushPackageToNuGetGallery -NuGetExecutableFilePath "$THIS_SCRIPTS_DIRECTORY\Older Version NuGet Executable\NuGet.exe" -Verbose
-#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Project\TestProject\TestProject.csproj" -VersionNumber "1.3" -ReleaseNotes "Pack project without building and specify version 1.3 and update the NuGet.exe" -NoPromptForPushPackageToNuGetGallery -NuGetExecutableFilePath "$THIS_SCRIPTS_DIRECTORY\Older Version NuGet Executable\NuGet.exe" -UpdateNuGetExecutable -Verbose
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project\TestProject\TestProject.csproj" -VersionNumber "1.3" -ReleaseNotes "Pack project without building and specify version 1.3 and do NOT update the NuGet.exe" -NoPromptForPushPackageToNuGetGallery -NuGetExecutableFilePath "$THIS_SCRIPTS_DIRECTORY\Older Version NuGet Executable\NuGet.exe" -Verbose
+#.\..\New-NuGetPackage.ps1 -ProjectFilePath "$THIS_SCRIPTS_DIRECTORY\Test Files\Test Project\TestProject\TestProject.csproj" -VersionNumber "1.3" -ReleaseNotes "Pack project without building and specify version 1.3 and update the NuGet.exe" -NoPromptForPushPackageToNuGetGallery -NuGetExecutableFilePath "$THIS_SCRIPTS_DIRECTORY\Older Version NuGet Executable\NuGet.exe" -UpdateNuGetExecutable -Verbose
 
 
 
 
-#Nuget spec $thisScriptsDirectory\TestProject\TestProject\TestProject.csproj -f
-#Nuget spec $thisScriptsDirectory\TestDll\TestDll.dll -f
+#Nuget spec $THIS_SCRIPTS_DIRECTORY\Test Files\TestProject\TestProject\TestProject.csproj -f
+#Nuget spec $THIS_SCRIPTS_DIRECTORY\Test Files\TestDll\TestDll.dll -f
 
-#Nuget pack $thisScriptsDirectory\TestProject\TestProject\TestProject.csproj -OutputDirectory $thisScriptsDirectory -Build
+#Nuget pack $THIS_SCRIPTS_DIRECTORY\Test Files\TestProject\TestProject\TestProject.csproj -OutputDirectory $thisScriptsDirectory -Build
 
-#cd $thisScriptsDirectory\TestProject\TestProject
+#cd $THIS_SCRIPTS_DIRECTORY\Test Files\TestProject\TestProject
 #nuget spec -f
 
-#cd $thisScriptsDirectory\TestDll
+#cd $THIS_SCRIPTS_DIRECTORY\Test Files\TestDll
 #nuget spec -f
